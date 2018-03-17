feb09862da0cdab75b2330423b0b2c42c712f7a8
Jakke Kuukkanen <jakke.kuukkanen@helsinki.fi>
2018-03-17 15:02:21 +0200

    tehty hakemistoille rakennetta

--- App.js
+++ App.js
@@ -1,55 +1,12 @@
 import React, { Component } from 'react'
+import Events from './components/Events'
 
-import { Calendar, LocaleConfig } from 'react-native-calendars'
-
-LocaleConfig.locales['fi'] = {
-    monthNames: [
-        'Tammikuu',
-        'Helmikuu',
-        'Maaliskuu',
-        'Huhtikuu',
-        'Toukokuu',
-        'Kesäkuu',
-        'Heinäkuu',
-        'Elokuu',
-        'Syyskuu',
-        'Lokakuu',
-        'Marraskuu',
-        'Joulukuu'],
-    monthNamesShort: [
-        'Tamm.',
-        'Helm.',
-        'Maal.',
-        'Huht.',
-        'Touk.',
-        'Kesä.',
-        'Hein.',
-        'Elok.',
-        'Syys.',
-        'Loka.',
-        'Marr.',
-        'Joul.'],
-    dayNames: [
-        'Maanantai',
-        'Tiistai',
-        'Keskiviikko',
-        'Torstai',
-        'Perjantai',
-        'Lauantai',
-        'Sunnuntai'],
-    dayNamesShort: ['Ma', 'Ti', 'Ke', 'To', 'Pe', 'La', 'Su'],
-}
-
-LocaleConfig.defaultLocale = 'fi'
-
-export default class CalendarsList extends Component {
+export default class App extends Component {
     constructor (props) {
         super(props)
     }
 
     render () {
-        return (
-            <Calendar current={'2018-01-01'}/>
-        )
+        return <Events />
     }
 }
